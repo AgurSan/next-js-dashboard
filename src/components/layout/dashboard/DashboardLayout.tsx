7fdd493ea54d9e34b85b58e50ebfbafaab1876f2
AgurSan <clement.sananikone@gmail.com>
2024-06-19 10:01:35 +0200

    build: file and folder restructuration, header/footer from ui to navigation

---
+++ src/components/layout/dashboard/DashboardLayout.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import React from 'react';
+import Footer from '../../navigation/footer';
+import Header from '../../navigation/header';
+
+export interface IDashboardLayout {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<IDashboardLayout> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>NextJs Fullstack Dashboard App</title>
+      </Head>
+      <div className="flex flex-col min-h-screen items-center">
+        <Header />
+        <main>{children}</main>
+        <div className="m-auto"></div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default DashboardLayout;
