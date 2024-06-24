7fb99cd37e70cba60f045dbfee2488cc566d0426
AgurSan <clement.sananikone@gmail.com>
2024-06-24 11:03:48 +0200

    build: implement Header structure

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -1,32 +1,110 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
 
-const Header: React.FC<IHeader> = ({ className, ...headerProps }) => {
+const Header: React.FC<IHeader> = () => {
   return (
-    <header
-      {...headerProps}
-      className={`w-full bg-white shadow-md py-4 ${className}`}
-    >
-      <div className="container mx-auto flex justify-between items-center px-4">
-        <Link href="/" className="text-xl font-bold text-blue-600">
-          Logo
-        </Link>
-        <nav className="space-x-4">
-          <Link
-            href="/"
-            className="hover:underline text-blue-600 font-semibold"
-          >
-            Home
-          </Link>
-          <Link href="/about" className="hover:underline text-gray-600">
-            About
-          </Link>
-          <Link href="/contact" className="hover:underline text-gray-600">
-            Contact
-          </Link>
-        </nav>
+    <header className="sticky top-0 z-100 flex w-full bg-white drop-shadow-1">
+      <div className="flex flex-grow items-center justify-between p-4 shadow-2 md:px-6 2xl:px-11 ">
+        <div className="hidden md:block">
+          <form>
+            <div className="relative">
+              <button className="absolute left-0 top-1/2 -translate-y-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </button>
+              <input
+                type="text"
+                placeholder="Type to search..."
+                className="w-full pl-8 pr-3 focus:outline-none bg-transparent"
+              ></input>
+            </div>
+          </form>
+        </div>
+        <div className="flex items-center gap-6">
+          <ul className="flex item-center gap-4">
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                />
+              </svg>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                />
+              </svg>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </li>
+          </ul>
+          <div>
+            <a className="flex items-center gap-4" href="#">
+              <span className="hidden text-right lg:block">
+                <span className="block text-sm font-medium text-black">
+                  Clément Sananikone
+                </span>
+                <span className="block text-xs font-medium">
+                  Web Developper
+                </span>
+              </span>
+              <Image
+                src="/images/user/avatar-01.png"
+                alt="User Avatar"
+                width={48}
+                height={48}
+                className="h-12 w-12 rounded-full"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
